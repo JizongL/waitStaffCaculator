fc23f297bbefe58b502d1fde85df148180c57977
tmmgeek <2015rpro@gmail.com>
2019-02-14 15:37:43 -0500

    meal-details customer charges and my Earning Info works,need to work on reset

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function renderCalculator(){
 
 
 
+
 function handleEnterMealDetails(){
     console.log('submit meal details to DATA');
     $('#js-enter-meal-details-form').submit(function(event){
@@ -32,23 +33,79 @@ function handleEnterMealDetails(){
         $('.js-tax-rate-entry').val('');
         const newTipPercentage = $('.js-tip-percentage').val();
         $('.js-tip-percentage').val('');
-        console.log(newBasePrice,newTaxRate,newTipPercentage);
-        addEntry(newBasePrice, newTaxRate,newTipPercentage);
+        console.log(newBasePrice,newTaxRate*0.01,newTipPercentage*0.01);
+        addEntry(Number(newBasePrice), Number(newTaxRate)*0.01,Number(newTipPercentage)*0.01);
         console.log(DATA);
+        handleCustomerCharge();
     });
     renderCalculator();
+    
 
     // handle meal details entry
     
 
 }
 
+function calculateSubtotal(basePrice,taxRate){
+    console.log('calculate subtotal');
+    return basePrice+(basePrice*taxRate);
+}
+
+function calculateTip(basePrice,TipPercentage){
+    return basePrice*TipPercentage;
+}
+
+function calculateTotal(subtotal,tip){
+    return subtotal+tip;
+}
+
+function updateCustomerCharges(subtotal,tip,total){
+    $('#subtotal-update').text(subtotal);
+    $('#tip-update').text(tip);
+    $('#total-update').text(total);
+}
+
 function handleCustomerCharge(){
     console.log('Display customer cahrges');
+    console.log('return last item of Data',DATA[DATA.length-1]);
+    const basePrice = DATA[DATA.length-1]['newBasePrice'];
+    const taxRate = DATA[DATA.length-1]['newTaxRate'];
+    const tipPercentage = DATA[DATA.length-1]['newTipPercentage'];
+    console.log('testing for calculation',basePrice,taxRate,tipPercentage);
+    const subtotal = calculateSubtotal(basePrice,taxRate);
+    const tip = calculateTip(basePrice,tipPercentage);
+    const total = subtotal+tip;
+    console.log('calcuated result',subtotal,tip,total);
+    updateCustomerCharges(subtotal,tip,total);
+    handleMyEarningInfo();
+}
+
+
+function tipTotal(){
+    console.log('tipTotal ran');
+}
+
+function mealCount(){
+    console.log('mealCount ran');
+}
+
+function averageTipPerMeal(){
+    console.log('averageTipPerMeal ran');
 }
 
 function handleMyEarningInfo(){
     console.log('display invidual earning info');
+    let tipTotal = 0;
+    let mealCount = 0;
+    
+    for(let item of DATA){
+        tipTotal+= (item['newBasePrice']*item['newTaxRate']);
+        mealCount += 1;
+    }
+    let averageTipPerMeal = tipTotal/mealCount;
+    $('#tip-total-update').text(tipTotal);
+    $('#meal-count-update').text(mealCount);
+    $('#average-tip-per-meal').text(averageTipPerMeal);
 }
 
 
