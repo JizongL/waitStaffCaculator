f73eb0fefa13e708432c6f3b57ab3053a623ed5a
tmmgeek <2015rpro@gmail.com>
2019-02-14 17:15:33 -0500

    fix html formatting and eslint index.js

--- index.js
+++ index.js
@@ -12,82 +12,82 @@ function addEntry(newBasePrice, newTaxRate,newTipPercentage) {
 // If reset button is clicked, empty DATA array, and reset all the values 
 // on Customer Charges and My Earnings Info
 function resetData(){
-    console.log('resetData ran ');
-    $('#reset-data').on('click',function(event){        
-        DATA=[];
-        $('#subtotal-update').text(0);
-        $('#tip-update').text(0);
-        $('#total-update').text(0);
-        $('#tip-total-update').text(0);
-        $('#meal-count-update').text(0);
-        $('#average-tip-per-meal').text(0);
-    });
-};
+  console.log('resetData ran ');
+  $('#reset-data').on('click',function(event){        
+    DATA=[];
+    $('#subtotal-update').text(0);
+    $('#tip-update').text(0);
+    $('#total-update').text(0);
+    $('#tip-total-update').text(0);
+    $('#meal-count-update').text(0);
+    $('#average-tip-per-meal').text(0);
+  });
+}
 
 // for test purpose, it logs DATA 
 function logArrayItems(){
-    console.log('testing: ',DATA);
-};
+  console.log('testing: ',DATA);
+}
 
 
 
 // handle new entry values of Meal details
 function handleEnterMealDetails(){
-    console.log('submit meal details to DATA');
-    $('#js-enter-meal-details-form').submit(function(event){
-        event.preventDefault();
-        const newBasePrice = $('.js-base-meal-price-entry').val();
-        $('.js-base-meal-price-entry').val('');
-        const newTaxRate = $('.js-tax-rate-entry').val();
-        $('.js-tax-rate-entry').val('');
-        const newTipPercentage = $('.js-tip-percentage').val();
-        $('.js-tip-percentage').val('');
-        // testing log
-        console.log(newBasePrice,newTaxRate*0.01,newTipPercentage*0.01);
-        // convert entry from string to number and add to Array.
-        addEntry(Number(newBasePrice), Number(newTaxRate)*0.01,Number(newTipPercentage)*0.01);
-        // testing log
-        console.log(DATA);
-        handleCustomerCharge();
-    });
-    // log array to confirm it is working,only for testing
-    logArrayItems();
+  console.log('submit meal details to DATA');
+  $('#js-enter-meal-details-form').submit(function(event){
+    event.preventDefault();
+    const newBasePrice = $('.js-base-meal-price-entry').val();
+    $('.js-base-meal-price-entry').val('');
+    const newTaxRate = $('.js-tax-rate-entry').val();
+    $('.js-tax-rate-entry').val('');
+    const newTipPercentage = $('.js-tip-percentage').val();
+    $('.js-tip-percentage').val('');
+    // testing log
+    console.log(newBasePrice,newTaxRate*0.01,newTipPercentage*0.01);
+    // convert entry from string to number and add to Array.
+    addEntry(Number(newBasePrice), Number(newTaxRate)*0.01,Number(newTipPercentage)*0.01);
+    // testing log
+    console.log(DATA);
+    handleCustomerCharge();
+  });
+  // log array to confirm it is working,only for testing
+  logArrayItems();
     
 
-    // handle meal details entry
+  // handle meal details entry
     
 
 }
 // calculate subtotal 
 function calculateSubtotal(basePrice,taxRate){
-    console.log('calculate subtotal');
-    return basePrice+(basePrice*taxRate);
+  console.log('calculate subtotal');
+  return basePrice+(basePrice*taxRate);
 }
 
 // calculate tip
 function calculateTip(basePrice,TipPercentage){
-    return basePrice*TipPercentage;
+  return basePrice*TipPercentage;
 }
 
 // calculate total
 function calculateTotal(subtotal,tip){
-    return subtotal+tip;
+  return subtotal+tip;
 }
 
 // update Customer Charges values
 function updateCustomerCharges(subtotal,tip,total){
-    $('#subtotal-update').text(subtotal);
-    $('#tip-update').text(tip);
-    $('#total-update').text(total);
+  $('#subtotal-update').text(subtotal);
+  $('#tip-update').text(tip);
+  $('#total-update').text(total);
 }
 
 // handle customer charge box details
 function handleCustomerCharge(){
-    // testing log
-    console.log('Display customer cahrges');
-    console.log('return last item of Data',DATA[DATA.length-1]);
-    // prevent if the initial list is empty would throw an error
-    if(DATA.length!==0){
+  // testing log
+  console.log('Display customer cahrges');
+  console.log('return last item of Data',DATA[DATA.length-1]);
+  // prevent if the initial list is empty would throw an error
+  if(DATA.length!==0){
     const basePrice = DATA[DATA.length-1]['newBasePrice'];
     const taxRate = DATA[DATA.length-1]['newTaxRate'];
     const tipPercentage = DATA[DATA.length-1]['newTipPercentage'];
@@ -109,26 +109,26 @@ function handleCustomerCharge(){
 
 
 function handleMyEarningInfo(){
-    console.log('display invidual earning info');
-    let tipTotal = 0;
-    let mealCount = 0;
-    // iterate through to claculate tipTotal and mealCount.
-    for(let item of DATA){
-        tipTotal+= (item['newBasePrice']*item['newTaxRate']);
-        mealCount += 1;
-    }
-    let averageTipPerMeal = tipTotal/mealCount;
-    // not a good bug patch, but when page is refreshed, DATA is empty, therefore
-    // when this function runs, it will return averageTipPerMeal as NaN. So, the `if`
-    // is to set averageTipPerMeal equals 0 if its value is NaN.
-    if (isNaN(averageTipPerMeal)){
-        $('#average-tip-per-meal').text(0);    
+  console.log('display invidual earning info');
+  let tipTotal = 0;
+  let mealCount = 0;
+  // iterate through to claculate tipTotal and mealCount.
+  for(let item of DATA){
+    tipTotal+= (item['newBasePrice']*item['newTaxRate']);
+    mealCount += 1;
+  }
+  let averageTipPerMeal = tipTotal/mealCount;
+  // not a good bug patch, but when page is refreshed, DATA is empty, therefore
+  // when this function runs, it will return averageTipPerMeal as NaN. So, the `if`
+  // is to set averageTipPerMeal equals 0 if its value is NaN.
+  if (isNaN(averageTipPerMeal)){
+    $('#average-tip-per-meal').text(0);    
     // else, update all values as normal
-    }else{
+  }else{
     $('#tip-total-update').text(tipTotal);
     $('#meal-count-update').text(mealCount);
     $('#average-tip-per-meal').text(averageTipPerMeal);
-    }
+  }
 }
 
 // main function to call all major handlers 
