7c280fac89691de54ecfd529720a6a2dc3c77df0
tmmgeek <2015rpro@gmail.com>
2019-02-15 22:34:24 -0500

    updated renderFunction customer charges and earning info calcluation, all calculations work now, need to put them to the html

--- index.js
+++ index.js
@@ -4,27 +4,65 @@
 
 let DATA = [];
 
-function addEntry(events, squirrel) {
-  DATA.push({events, squirrel});
+function addEntry(newBasePrice, newTaxRate,newTipPercentage) {
+  DATA.push({newBasePrice, newTaxRate,newTipPercentage});
+}
+
+
+function generateRightColumnBoxes(){
+    console.log('generate html element for the right column');
+}
+
+function renderCalculator(){
+    console.log('`renderCalculator` ran');
+    $('.result-display-column').html(
+        `<div class="customer-charge-box">
+        <div class = "box box-top orange">
+            <h2>Customer Charges</h2>
+        </div>    
+
+        <div class = "box box-details">
+            <p>Subtotal: <span>0.00</span></p>
+            <p>Tip: <span>0.00</span></p>
+            <hr>
+            <p>total: <span>0.00</span></p>
+        </div>
+    </div>
+<!---->
+    <div class = "earning-info-box">
+    
+            <div class = "box box-top orange">
+                <h2>My Earnings Info</h2>
+            </div>   
+            <div class = "box box-details">
+                    <p>Tip Total: <span>0.00</span></p>
+                    <p>Meal count: <span>0.00</span></p>            
+                    <p>Average Tip Per Meal: <span>0.00</span></p>
+                </div>
+            </div>
+`
+    );
 }
 
 
 
-
-
+        
 
 function handleEnterMealDetails(){
-    console.log('submit meal details to DATA');
+    console.log('`handleEnterMealDetails` ran');
     $('#js-enter-meal-details-form').submit(function(event){
         event.preventDefault();
-        const newBasePrice = $('.js-base-meal-price-entry').val();
+        const newBasePrice = Number($('.js-base-meal-price-entry').val());
         $('.js-base-meal-price-entry').val('');
-        const newTaxRate = $('.js-tax-rate-entry').val();
+        const newTaxRate = Number($('.js-tax-rate-entry').val());
         $('.js-tax-rate-entry').val('');
-        const newTipPercentage = $('.js-tip-percentage').val();
+        const newTipPercentage = Number($('.js-tip-percentage').val());
         $('.js-tip-percentage').val('');
-        console.log(newBasePrice,newTaxRate,newTipPercentage);
-        
+        console.log('new entry',newBasePrice,newTaxRate,newTipPercentage);
+        addEntry(newBasePrice,newTaxRate*0.01,newTipPercentage*0.01);
+        // temp, it will be moved to renderCalculator() later.
+        handleCustomerCharge();
+        handleMyEarningInfo();
     });
 
 
@@ -33,12 +71,52 @@ function handleEnterMealDetails(){
 
 }
 
+// calculate subtotal 
+function calculateSubtotal(basePrice,taxRate){
+    console.log('calculate subtotal');
+    return basePrice+(basePrice*taxRate);
+  }
+  
+  // calculate tip
+  function calculateTip(basePrice,TipPercentage){
+    return basePrice*TipPercentage;
+  }
+  
+function customerChargeCalucation(){
+    console.log('`customerChargeCalucation` ran');
+    if(DATA.length !==0){
+        //let subtotal = calculateSubtotal(DATA[DATA.length-1].newBasePrice,DATA[DATA.length-1].newTaxRate);
+        console.log('testing data',DATA);
+        let subtotal = calculateSubtotal(DATA[DATA.length-1].newBasePrice,DATA[DATA.length-1].newTaxRate);
+        console.log('testing subtotal calculation',subtotal);
+        let tip = calculateTip(DATA[DATA.length-1].newBasePrice,DATA[DATA.length-1].newTipPercentage);
+        console.log('testing tip calcluation',tip);
+        let total = subtotal+tip;
+        return [subtotal,tip,total];
+      } else{
+          return 'no calculation available';
+      }
+}
+
 function handleCustomerCharge(){
-    console.log('Display customer cahrges');
+    console.log('`handleCustomerCharge` ran');
+    let resultArray = customerChargeCalucation();
+    console.log(resultArray);
+    
 }
 
 function handleMyEarningInfo(){
-    console.log('display invidual earning info');
+    console.log('`handleMyEarningInfo` ran');
+    if(DATA.length!==0){
+    let tipTotal = 0;
+    for (let meal of DATA){
+        tipTotal+= meal.newBasePrice*meal.newTipPercentage;
+    }
+    let mealCount = DATA.length;
+    let averageTipPerMeal = tipTotal/mealCount;
+    console.log('testing earning info cal',tipTotal,mealCount,averageTipPerMeal);
+    }    
+
 }
 
 
@@ -46,6 +124,7 @@ function waitStaff_calculator(){
   handleEnterMealDetails();
   handleCustomerCharge();
   handleMyEarningInfo();
+  renderCalculator();
 }
 
 $(waitStaff_calculator);
